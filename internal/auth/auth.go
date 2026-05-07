c872862ec0a33f4534b97b91f125102b5bb9943d
Yuto Isayama <isayamayuto@icloud.com>
2026-05-07 17:26:24 +0900

    Basic認証用/authに作成

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,14 +1,30 @@
 package auth
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
+// 共通の認証インターフェース
 type Authenticator interface {
 	Apply(req *http.Request) error
 }
 
-// のちに実装
+// 認証なし(プロトタイプ用)
 type NoAuth struct{}
 
 func (n *NoAuth) Apply(req *http.Request) error {
+	return nil
+}
+
+// ベーシック認証
+type BearerAuth struct {
+	Token string
+}
+
+func (b *BearerAuth) BasicApply(req *http.Request) error {
+	if b.Token != "" {
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", b.Token))
+	}
 	return nil
 }
--- internal/scanner/e2e_test.go
+++ internal/scanner/e2e_test.go
@@ -45,6 +45,7 @@ func TestScanner_E2E_BOLA_Detection(t *testing.T) {
 		SizeWeight:      10,
 	}
 	gen := generator.NewSequentialGenerator(100, 102, "")
+	// テストが通らない
 	at := &auth.NoAuth{}
 
 	s := NewScanner(ts.URL+"/api/users/{{ID}}", ev, gen, at, 2, client)

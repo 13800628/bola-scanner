27f2b78ac4dc2c2d1616bf0ad14caa2a91282305
Yuto Isayama <isayamayuto@icloud.com>
2026-05-14 17:50:56 +0900

    比較機能プロトタイプ

--- internal/generator/pair.go
+++ internal/generator/pair.go
@@ -1,10 +1,41 @@
 package generator
 
 type PairGenerator struct {
-	ids []string
+	ids []string // 全IDリスト
 	i   int
 	j   int
 }
 
-func NewPairGenerator(ids []string) *PairGenerator
-func (g *PairGenerator) Next() (victim, attacker string, hasNext bool)
+func NewPairGenerator(ids []string) *PairGenerator {
+	return &PairGenerator{
+		ids: ids,
+		i:   0,
+		j:   1,
+	}
+}
+
+func (g *PairGenerator) Next() (victim, attacker string, hasNext bool) {
+	// IDが一つしかない
+	if len(g.ids) < 2 {
+		return "", "", false
+	}
+
+	// iが最後まで来てしまった(ペアがいない)
+	if g.i >= len(g.ids)-1 {
+		return "", "", false
+	}
+
+	victim = g.ids[g.i]
+	attacker = g.ids[g.j]
+
+	g.j++
+
+	if g.j >= len(g.ids) {
+		g.i++
+		g.j = g.i + 1
+	}
+
+	hasNext = g.i < len(g.ids)-1
+
+	return victim, attacker, hasNext
+}

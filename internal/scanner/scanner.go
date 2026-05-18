2023c4c2e97f43fbaa93e4046f26d92abc8f9c97
Yuto Isayama <isayamayuto@icloud.com>
2026-05-18 16:39:43 +0900

    scanOneのbuildURLの引数を正しい方に修正

--- internal/scanner/scanner.go
+++ internal/scanner/scanner.go
@@ -72,7 +72,7 @@ func (s *Scanner) PrepareVictimData(victimAuth auth.Authenticator, victimID stri
 }
 
 func (s *Scanner) scanOne(job ScanJob) (ScanResult, error) {
-	targetURL := s.buildURL(job.VictimID)
+	targetURL := s.buildURL(job.AttackerID)
 	req, err := http.NewRequest("GET", targetURL, nil)
 	if err != nil {
 		return ScanResult{}, err

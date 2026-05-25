5e31b074eb541796a2632593484f84cdb2448168
Yuto Isayama <isayamayuto@icloud.com>
2026-05-25 12:03:57 +0900

    コメント修正

--- internal/generator/pair.go
+++ internal/generator/pair.go
@@ -15,12 +15,12 @@ func NewPairGenerator(ids []string) *PairGenerator {
 }
 
 func (g *PairGenerator) Next() (victim, attacker string, hasNext bool) {
-	// IDが一つしかない
+	// IDが一つしかない部分でのガード節
 	if len(g.ids) < 2 {
 		return "", "", false
 	}
 
-	// iが最後まで来てしまった(ペアがいない)
+	// iが最後まで来てしまった(ペアがいない)のガード節
 	if g.i >= len(g.ids)-1 {
 		return "", "", false
 	}

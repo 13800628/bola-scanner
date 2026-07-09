4c774f81036840511a099e645651d96cae543a5f
Yuto Isayama <isayamayuto@icloud.com>
2026-07-09 16:27:03 +0900

    修正

--- internal/scanner/scanner.go
+++ internal/scanner/scanner.go
@@ -115,6 +115,7 @@ func (s *Scanner) Run() []ScanResult {
 	resultChan := make(chan ScanResult)
 	var wg sync.WaitGroup
 
+	// 深いから修正余地か
 	// ワーカーの起動
 	for i := 0; i < s.WorkerCount; i++ {
 		wg.Add(1)
